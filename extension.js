075dead076deb14c9240e089a60a071f1f97a1f5
creepersaur <69436784+creepersaur@users.noreply.github.com>
2026-06-01 09:53:31 +0530

    even more logs

--- extension.js
+++ extension.js
@@ -3,27 +3,44 @@ console.log("TORRENTIO MODULE LOADED")
 export default new class Torrentio {
 	// gh:creepersaur/HayaseTorrentio
 
-	async single({ titles, episode }) {
-		console.log("TORRENTIO: single()")
+	// async single({ titles, episode }) {
+	// 	console.log("TORRENTIO: single()")
 
-		return [
-			{
-				hash: "0123456789ABCDEF0123456789ABCDEF01234567",
-				link: "0123456789ABCDEF0123456789ABCDEF01234567",
-				title: "creepersaur",
-				size: 1,
-				type: "alt",
-				date: new Date(),
-				seeders: 1,
-				leechers: 0,
-				downloads: 0,
-				accuracy: "medium"
-			}
-		]
-	}
+	// 	return [
+	// 		{
+	// 			hash: "0123456789ABCDEF0123456789ABCDEF01234567",
+	// 			link: "0123456789ABCDEF0123456789ABCDEF01234567",
+	// 			title: "creepersaur",
+	// 			size: 1,
+	// 			type: "alt",
+	// 			date: new Date(),
+	// 			seeders: 1,
+	// 			leechers: 0,
+	// 			downloads: 0,
+	// 			accuracy: "medium"
+	// 		}
+	// 	]
+	// }
 
-	batch = this.single
-	movie = this.single
+	async single(query) {
+        console.log("single", query)
+        return []
+    }
+
+    async batch(query) {
+        console.log("batch", query)
+        return []
+    }
+
+    async movie(query) {
+        console.log("movie", query)
+        return []
+    }
+
+    async test() {
+        console.log("test")
+        return true
+    }
 
 	async test() {
 		return true
